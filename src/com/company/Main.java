8fa8bbebaf4ff6d3f4a9db9520992ab2dfe17373
Majkes <Majk3s@wp.pl>
2021-02-24 13:12:45 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 10;
+        while (i <= 20) {
+            System.out.println(i);
+
+            i++;
+        }
+    }
+}
+
+
