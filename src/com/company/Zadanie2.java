5f7ab4451f33ab2b7de0a763d710bf20bb955e6e
Majkes <Majk3s@wp.pl>
2021-02-24 13:31:31 +0100

    commit

---
+++ src/com/company/Zadanie2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+
+        for (int i = -10; i <= 40; i++) {
+
+            if(i%2==1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
